1947dbc6f8c69f8e317ec9d910a2d6c128c95418
Mohanapriya <priya05mca@gmail.com>
2019-04-17 21:59:31 +0100

    commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+
+// $('#view-work').on('click', function() {
+// 	const images$('#images').position().top;
+
+// 	$('html,body').animate(
+// 	{
+// 	scrollTop: images
+// 	},
+// 	900
+// 	);
+// });
+// });
